eab4066779a2ce33d3d6f94e721ef8b8cc1e0ddc
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-05-12 17:43:15 +0200

    CRUD for books

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Book} from '../models/book';
+import {Subscription} from 'rxjs/Subscription';
+import {BooksService} from '../services/books.service';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
+    selector: 'app-book-list',
+    templateUrl: './book-list.component.html',
+    styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
 
-  constructor() { }
+export class BookListComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {
-  }
+    books: Book[];
+    booksSubscription: Subscription;
 
+    constructor(private booksService: BooksService, private router: Router) {
+    }
+
+    ngOnInit() {
+        this.booksSubscription = this.booksService.booksSubject.subscribe(
+            (books: Book[]) => {
+                this.books = books;
+            }
+        );
+        this.booksService.emitBooks();
+    }
+
+    onNewBook() {
+        this.router.navigate(['/books', 'new']);
+    }
+
+    onDeleteBook(book: Book) {
+        this.booksService.removeBook(book);
+    }
+
+    onViewBook(id: number) {
+        this.router.navigate(['/books', 'view', id]);
+    }
+
+    ngOnDestroy() {
+        this.booksSubscription.unsubscribe();
+    }
 }
