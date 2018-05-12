26ee63e170e986766a88fd7478802c063beae34e
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-05-12 16:49:12 +0200

    Config for Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+    constructor() {
+        const config = {
+            apiKey: 'AIzaSyCph3rQiaJhvuB9cvqMhw7-mENtq2sP24w',
+            authDomain: 'bibliotheque-28281.firebaseapp.com',
+            databaseURL: 'https://bibliotheque-28281.firebaseio.com',
+            projectId: 'bibliotheque-28281',
+            storageBucket: 'bibliotheque-28281.appspot.com',
+            messagingSenderId: '497361091282'
+        };
+        firebase.initializeApp(config);
+    }
 }
